2750294d74476b53ab273737b189ab17452da11a
vishalglobussoftSDE <vishalvishwakarma@globussoft.in>
2025-05-22 13:52:30 +0530

    configDB and write usercontroller

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import userRoutes from './routes/user.routes.js'
+import connectDB from './config/db.config.js';
 // Load environment variables
 const initServer = ()=>{
     dotenv.config();
-    
+    connectDB();
     const app = express();
     const PORT = process.env.PORT || 3000;
     
@@ -15,7 +16,7 @@ const initServer = ()=>{
     app.get('/', (req, res) => {
       res.send('Hello World!');
     });
-    
+    app.use('/auth' , userRoutes);
     app.listen(PORT, () => {
       console.log(`Server is running on port http://localhost:${PORT}`);
     });
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,9 +1,20 @@
 // controllers/user.controller.js
-
-export const register = (req, res) => {
-  res.send('User registration endpoint');
+import UserModel from "../models/user.model.js";
+export const register = async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.send('all fields req');
+    }
+    try {
+      const user = new UserModel({ email, password });
+      await user.save();
+      res.send(`user registered with email: ${user.email} and password: ${user.password}`);
+    } catch (error) {
+      res.status(500).send('Error registering user: ' + error);
+      
+    }
 };
 
-export const login = (req, res) => {
+export const login = (res) => {
   res.send('User login endpoint');
 };
